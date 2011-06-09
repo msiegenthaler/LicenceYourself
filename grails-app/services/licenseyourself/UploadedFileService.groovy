fe64e476a0f3f9bd41a3c00f67b12c18ba12d65c
msiegenthaler <msiegenthaler@gmx.net>
2011-06-09 23:11:26 +0200

    don't download as attachment, just as file

--- grails-app/services/licenseyourself/UploadedFileService.groovy
+++ grails-app/services/licenseyourself/UploadedFileService.groovy
@@ -4,7 +4,7 @@ class UploadedFileService {
 
 	def streamToClient(UploadedFile file, resp) {
 		def fileName = URLEncoder.encode(file.name)
-		resp.addHeader("content-disposition", "attachment;filename=$fileName")
+		resp.addHeader("content-disposition", "filename=$fileName")
 		resp.contentType = file.mimeType
 		resp.contentLength = file.content.size()
 		resp.outputStream << file.content
