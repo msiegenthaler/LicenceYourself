13e1ad451c2373eb72e85741c4af9402b702de69
msiegenthaler <msiegenthaler@gmx.net>
2011-07-28 08:59:06 +0200

    basic installation of the spring security plugin (incl. ldap)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -88,3 +88,8 @@ log4j = {
 
     warn   'org.mortbay.log'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'licenseyourself.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'licenseyourself.UserRole'
+grails.plugins.springsecurity.authority.className = 'licenseyourself.Role'
